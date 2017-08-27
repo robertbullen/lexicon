b4b2f447474a008e221e36393ccc65a62b637e62
Robert Bullen <robert@robertbullen.com>
2017-08-27 11:21:45 -0500

    * updated all packages to the latest * set `alexaObject.appId`

--- index.ts
+++ index.ts
@@ -4,14 +4,15 @@ import * as lambda from 'aws-lambda';
 import * as handlers from './src/handlers';
 
 const handler: lambda.Handler = (
-    event: alexa.RequestBody,
+    event: alexa.RequestBody<any>,
     context: lambda.Context & alexa.Context,
     _callback: lambda.Callback // tslint:disable-line:variable-name
 ) => {
     console.log(JSON.stringify(event, null, 2));
 
     // Construct an AlexaObject and execute it.
-    const alexaObject: alexa.AlexaObject = alexa.handler(event, context);
+    const alexaObject: alexa.AlexaObject<any> = alexa.handler(event, context);
+    alexaObject.appId = 'amzn1.ask.skill.5350392a-8244-473a-85ac-81bfe7034fb9';
     alexaObject.registerHandlers(handlers.handlers);
     alexaObject.execute();
 };
